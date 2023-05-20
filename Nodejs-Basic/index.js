dfd1809e35c51905c8b473046670ddd19eae7164
--replace-all <imaryan08@gmail.com>
2023-05-20 13:31:41 +0530

    crypto function added

--- Nodejs-Basic/index.js
+++ Nodejs-Basic/index.js
@@ -1,4 +1,14 @@
-const { add, sub, mul, div, sine, cos, tan } = require("./operations");
+const {
+  add,
+  sub,
+  mul,
+  div,
+  sine,
+  cos,
+  tan,
+  generateRandomNumber,
+  randomInteger,
+} = require("./operations");
 
 add(4, 5);
 sub(14, 5);
@@ -7,3 +17,5 @@ mul(14, 5);
 sine(90);
 cos(30);
 tan(45);
+generateRandomNumber(16);
+randomInteger(10);
