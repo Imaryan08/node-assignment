082ac665ea406b8d5a8bdad4b1424ecf2925feb5
--replace-all <imaryan08@gmail.com>
2023-05-24 12:39:38 +0530

    route added for student controller and batch controller

--- express_MVC/src/index.js
+++ express_MVC/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const userController = require("./controllers/user.controller");
-const studentController = require("./controllers/studentCOntroller");
+const studentController = require("./controllers/student.controller");
+const batchController = require("./controllers/batch.controller");
+const evaluationController = require("./controllers/evaluation.controller");
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/users", userController);
 app.use("/students", studentController);
+app.use("/batches", batchController);
+app.use("/evaluations", evaluationController);
 
 module.exports = app;
