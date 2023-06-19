b0036b51859e6a10ccc5f7d9a58156e168a934a3
--replace-all <imaryan08@gmail.com>
2023-06-19 11:32:30 +0530

    get the cookie value

--- cookie/app.js
+++ cookie/app.js
@@ -4,13 +4,23 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Welcome to backend!");
 });
 
+// Set ccokie value
 app.get("/setcookie", (req, res) => {
   res.cookie("mycookie", "cookie Value");
   res.status(200).send("cookie has been set!");
+});
+
+// Read cookie value
+app.get("/getcookie", (req, res) => {
+  const myCookie = req.cookies.mycookie;
+  console.log(myCookie);
+  res.send("cookie read successfully");
 });
 
 app.listen(8080, () => {
