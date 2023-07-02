ace90de636110412b2673ff093fac7caaabd76d2
--replace-all <imaryan08@gmail.com>
2023-07-02 13:09:55 +0530

    index file added

--- pair up/server/index.js
+++ pair up/server/index.js
@@ -15,4 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/register", register);
 app.post("/login", login);
 app.get("/dashboard", authenticateToken, dashboard);
+
+// event
+
+app.post("/event", require("./src/controllers/event.controller"));
 module.exports = app;
